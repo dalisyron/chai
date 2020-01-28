8cecfdde1aa1e6e0d4630ce3de221a012550b563
Mobin Dariush <dalisyron@gmail.com>
2020-01-28 11:47:09 +0330

    Add alpha beta

--- src/AlphaBetaAI.py
+++ src/AlphaBetaAI.py
@@ -1,10 +1,98 @@
 import chess
 from math import inf
 
+value_dict = {
+    'p': 1,
+    'n': 3,
+    'b': 3,
+    'r': 5,
+    'q': 9,
+    'k': 0,
+    'none': 0,
+}
+
+def evaluation_function(board):
+
+    result = 0
+
+    for file in range(8):
+        for rank in range(8):
+            piece = board.piece_at(chess.square(file, rank))
+            result += value_dict[piece.__str__().lower()]
+
+    return result
+
+INF = 100000000000
 
 class AlphaBetaAI():
-    def __init__(self, depth):
-        pass
+    def __init__(self, index, depth):
+        self.depth = depth
+        self.index = index
 
-    def choose_move(self, board):
-        pass
+    def choose_move(self, board: chess.Board):
+        minimaxScores = []
+        legalActions = list(board.legal_moves)
+
+
+        alpha = -INF
+        beta = INF
+
+        for move in legalActions:
+            board.push(move)
+            score = self.minimax(board, self.index, 0, alpha, beta)
+            board.pop()
+            minimaxScores.append((score, move))
+
+        max_move = None
+        max_move_value = -INF
+
+        for scoreMove in minimaxScores:
+            if (scoreMove[0] > max_move_value):
+                max_move_value = scoreMove[0]
+                max_move = scoreMove[1]
+
+        return max_move
+
+    def minimax(self, board, maximizingAgent, currentDepth, alpha, beta):
+
+        if currentDepth == self.depth:
+            return evaluation_function(board)
+
+        if board.is_checkmate() or board.is_stalemate() or board.is_repetition():
+            return -100000000 if board.turn == self.index else 100000000
+
+        if currentDepth % 2 == 0:
+            return self.min_value(board, self.getOpponentIndex(), currentDepth, alpha, beta)
+        else:
+            return self.max_value(board, maximizingAgent, currentDepth, alpha, beta)
+
+    def min_value(self, board, minimizingAgent, depth, alpha, beta):
+        v = 1000000000000
+        moves = list(board.legal_moves)
+
+        for move in moves:
+            board.push(move)
+            v = min(v, self.minimax(board, minimizingAgent, depth + 1, alpha, beta))
+            board.pop()
+            if (v <= alpha):
+                return v
+            beta = min(beta, v)
+
+        return v
+
+    def max_value(self, board, maximizingAgent, depth, alpha, beta):
+        v = -1000000000000
+        moves = list(board.legal_moves)
+
+        for move in moves:
+            board.push(move)
+            v = max(v, self.minimax(board, maximizingAgent, depth + 1, alpha, beta))
+            board.pop()
+            if (v >= beta):
+                return v
+            alpha = max(alpha, v)
+
+        return v
+
+    def getOpponentIndex(self):
+        return chess.COLORS[1 - chess.COLORS.index(self.index)]
--- src/test_chess.py
+++ src/test_chess.py
@@ -11,14 +11,14 @@ from ChessGame import ChessGame
 
 import sys
 
-
-player1 = HumanPlayer()
-player2 = RandomAI()
+player1 = MinimaxAI(chess.WHITE, 2)
+player2 = MinimaxAI(chess.BLACK, 2)
 
 game = ChessGame(player1, player2)
 
 while not game.is_game_over():
     print(game)
+    print(game.board.is_checkmate(), game.board.is_stalemate(), game.board.is_repetition())
     game.make_move()
 
 
